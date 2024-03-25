42f3421a18845996dc1f9e8567678c0605de470b
Daniel Osawaru <35234154+dosawaru@users.noreply.github.com>
2024-03-25 12:09:19 -0400

    implemented ability to add category data to the database

--- BookHavenWeb/Controllers/CategoryController.cs
+++ BookHavenWeb/Controllers/CategoryController.cs
@@ -17,5 +17,20 @@ namespace BookHavenWeb.Controllers
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+        //Get
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
