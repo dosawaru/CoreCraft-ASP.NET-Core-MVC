708db74401584626b03266e2a5b663916e170be7
Daniel Osawaru <35234154+dosawaru@users.noreply.github.com>
2024-03-25 13:34:50 -0400

    added abililty to edit data

--- BookHavenWeb/Controllers/CategoryController.cs
+++ BookHavenWeb/Controllers/CategoryController.cs
@@ -43,5 +43,40 @@ namespace BookHavenWeb.Controllers
             return View(obj);
 
         }
+
+        //Get
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id== 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Id == id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+            return View(categoryFromDb);
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The DisplayOrder cannot exactly match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+
+        }
     }
 }
